90cbbfe50d4e0ba11acc8501c782326dca04bb87
Marko Srdanovic <marko.srdanovic@gmail.com>
2008-06-24 20:40:31 +0000

    Add utiltiy class that will attempt to get the Implementation-Version attribute from the manfest for a given class.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/icb-commons/icb-utils@16182 60cd2ccc-2b05-0410-9566-da273a940bff

---
+++ src/edu/cornell/med/icb/util/VersionUtils.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2008 Institute for Computational Biomedicine,
+ *                    Weill Medical College of Cornell University
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package edu.cornell.med.icb.util;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.net.URL;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
+/**
+ * Utilities to deal with version numbers of source code.
+ */
+public final class VersionUtils {
+    /**
+     * Used to log debug and informational messages.
+     */
+    private static final Log LOG = LogFactory.getLog(VersionUtils.class);
+
+    /**
+     * Private constructor for utility class.
+     */
+    private VersionUtils() {
+        super();
+    }
+
+    /**
+     * Gets the Implementation-Version attribute from the manifest of the jar file a class
+     * is loaded from.
+     * @param clazz The class to get the version for
+     * @return The value of the Implementation-Version attribute or "UNKNOWN" if the
+     * jar file cannot be read.
+     */
+    public static String getImplementationVersion(final Class<?> clazz) {
+        String version;
+        try {
+            final String classContainer =
+                    clazz.getProtectionDomain().getCodeSource().getLocation().toString();
+            final URL manifestUrl = new URL("jar:" + classContainer + "!/" + JarFile.MANIFEST_NAME);
+            final Manifest manifest = new Manifest(manifestUrl.openStream());
+            final Attributes attributes = manifest.getMainAttributes();
+            version = attributes.getValue(Attributes.Name.IMPLEMENTATION_VERSION);
+        } catch (Exception e) {
+            // pretty much any error here is ok since we may not even have a jar to read from
+            version = "UNKNOWN";
+        }
+
+        LOG.info(clazz.getName() + " " + Attributes.Name.IMPLEMENTATION_VERSION + ": " + version);
+        return StringUtils.defaultString(version);
+    }
+}
---
+++ test-src/edu/cornell/med/icb/util/TestVersionUtils.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) ${YEAR} Institute for Computational Biomedicine,
+ *                    Weill Medical College of Cornell University
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package edu.cornell.med.icb.util;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.SystemUtils;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Validates functionality of {@link VersionUtils}.
+ */
+public class TestVersionUtils {
+    /**
+     * Default constructor.
+     */
+    public TestVersionUtils() {
+        super();
+    }
+
+    /**
+     * Validates functionality of {@link VersionUtils#getImplementationVersion(Class<?> )}.
+     */
+    @Test
+    public void getObjectImplementationVersion() {
+        final String version = VersionUtils.getImplementationVersion(SystemUtils.class);
+        assertNotNull("Version number should never be null", version);
+        assertTrue("Version number should be at least 2.3", Double.parseDouble(version) >= 2.3);
+    }
+
+    /**
+     * Validates functionality of {@link VersionUtils#getImplementationVersion(Class<?> )}
+     * with a class that has an unknown version.
+     */
+    @Test
+    public void getUnknownImplementationVersion() {
+        // this should give us unknown since there should be no jar for tests
+        final String version = VersionUtils.getImplementationVersion(this.getClass());
+        assertNotNull("Version number should never be null", version);
+        assertEquals("Version number should be unknown", "UNKNOWN", version);
+    }
+
+    /**
+     * Validates functionality of {@link VersionUtils#getImplementationVersion(Class<?> )}
+     * with a null class.
+     */
+    @Test(expected = NullPointerException.class)
+    public void getNullImplementationVersion() {
+        final String version = VersionUtils.getImplementationVersion(null);
+        assertNotNull("Version number should never be null", version);
+        assertEquals("Version number should equal the runtime version",
+                SystemUtils.JAVA_RUNTIME_VERSION, version);
+    }
+
+    /**
+     * Validates functionality of {@link VersionUtils#getImplementationVersion(Class<?> )}
+     * with a jar is readable but that has no implementation version attribute.
+     */
+    @Test
+    public void getNoImplementationVersion() {
+        final String version = VersionUtils.getImplementationVersion(org.junit.Test.class);
+        assertNotNull("Version number should never be null", version);
+        assertEquals("Version number should be empty", StringUtils.EMPTY, version);
+    }
+
+}
