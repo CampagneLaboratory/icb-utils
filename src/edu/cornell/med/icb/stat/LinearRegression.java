004de6701d1ac1c2ce4ae0a230fb262382829245
Kevin C. Dorf <kdorff@campagnelab.org>
2008-11-07 17:44:50 +0000

    A simple linear regression class added. I will use this with the ProcessEstimator.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/icb-commons/icb-utils@17290 60cd2ccc-2b05-0410-9566-da273a940bff

---
+++ src/edu/cornell/med/icb/stat/LinearRegression.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright (C) 2008 Institute for Computational Biomedicine,
+ *                    Weill Medical College of Cornell University
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package edu.cornell.med.icb.stat;
+
+/**
+ * This class performs a Linear Regression. Based partly on the code found within
+ * RegressionApplet.java found at  
+ * http://www.math.csusb.edu/faculty/stanton/m262/regress/regress.html
+ */
+public class LinearRegression {
+    /** The number of data points. */
+    private int numberDataPoints;
+
+    /** The sum of all data point x*x values. */
+    private int sumxx;
+
+    /** The sum of all data point x*y values. */
+    private int sumxy;
+
+    /** The sum of all data point x values. */
+    private int sumx;
+
+    /** The sum of all data point y values. */
+    private int sumy;
+
+    /** The x-intercept as calculated in regress(). */
+    private double xIntercept;
+
+    /** The y-intercept as calculated in regress(). */
+    private double yIntercept;
+
+    /** The slope as calculated in regress(). */
+    private double slope;
+
+    /**
+     * Create a linear regression calculator.
+     */
+    public LinearRegression() {
+        reset();
+    }
+
+    /**
+     * Add a point to the linear regression calculation.
+     * @param x the x of the point being added
+     * @param y the y of the point being added
+     */
+    public void addDataPoint(final int x, final int y) {
+        numberDataPoints++;
+        sumx += x;
+        sumy += y;
+        sumxx += x * x;
+        sumxy += x * y;
+    }
+
+    /**
+     * Prepare for a new regression calculation.
+     */
+    public void reset() {
+        xIntercept = Double.NaN;
+        yIntercept = Double.NaN;
+        slope = Double.NaN;
+
+        numberDataPoints = 0;
+        sumxx = 0;
+        sumxy = 0;
+        sumx = 0;
+        sumy = 0;
+    }
+
+    /**
+     * Run the regression. This should be done before calling
+     * any of getXIntercept(), getYIntercept(), or getSlope().
+     */
+    public void regress() {
+        if (numberDataPoints > 1) {
+            final double Sxx = sumxx - sumx * sumx / numberDataPoints;
+            final double Sxy = sumxy - sumx * sumy / numberDataPoints;
+            slope = Sxy / Sxx;
+            yIntercept = (sumy - slope * sumx) / numberDataPoints;
+            xIntercept = (-yIntercept) / slope;
+        }
+    }
+
+    /**
+     * Obtain the x-intercept. regress() should be called before calling
+     * any of getXIntercept(), getYIntercept(), or getSlope().
+     * @return the x-intercept
+     */
+    public double getXIntercept() {
+        return this.xIntercept;
+    }
+
+    /**
+     * Obtain the y-intercept. regress() should be called before calling
+     * any of getXIntercept(), getYIntercept(), or getSlope().
+     * @return the x-intercept
+     */
+    public double getYIntercept() {
+        return this.yIntercept;
+    }
+
+    /**
+     * Obtain the slope. regress() should be called before calling
+     * any of getXIntercept(), getYIntercept(), or getSlope().
+     * @return the slope
+     */
+    public double getSlope() {
+        return this.slope;
+    }
+}
---
+++ test-src/edu/cornell/med/icb/stat/TestLinearRegression.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2008 Institute for Computational Biomedicine,
+ *                    Weill Medical College of Cornell University
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package edu.cornell.med.icb.stat;
+
+import org.junit.Test;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Test the LinearRegression class. 
+ * @author Kevin Dorff
+ */
+public class TestLinearRegression {
+    /**
+     * Negative slope regression.
+     */
+    @Test
+    public void testLinearRegression() {
+        final LinearRegression reg = new LinearRegression();
+        reg.addDataPoint(10, 3);
+        reg.addDataPoint(20, 2);
+        reg.addDataPoint(30, 1);
+        reg.regress();
+        assertEquals(40.0d, reg.getXIntercept());
+        assertEquals(4.0d, reg.getYIntercept());
+        assertEquals(-0.1d, reg.getSlope());
+    }
+
+    /**
+     * Another negative slope regression, but this time check after regressing just one point,
+     * then add another point but don't regress and check, finally regress and check that
+     * the value is correct.
+     */
+    @Test
+    public void testLinearRegression2() {
+        final LinearRegression reg = new LinearRegression();
+        reg.addDataPoint(10, 3);
+        // Cannot regress with one point
+        reg.regress();
+        assertTrue(Double.isNaN(reg.getXIntercept()));
+        assertTrue(Double.isNaN(reg.getYIntercept()));
+        assertTrue(Double.isNaN(reg.getSlope()));
+        reg.addDataPoint(30, 2);
+        // Did not call regress() after adding the point
+        assertTrue(Double.isNaN(reg.getXIntercept()));
+        assertTrue(Double.isNaN(reg.getYIntercept()));
+        assertTrue(Double.isNaN(reg.getSlope()));
+        reg.regress();
+        // Now things are correct, we can regress with just two points
+        assertEquals(70.0d, reg.getXIntercept());
+        assertEquals(3.5d, reg.getYIntercept());
+        assertEquals(-0.05d, reg.getSlope());
+    }
+
+    /**
+     * Positive slope regression.
+     */
+    @Test
+    public void testLinearRegression3() {
+        final LinearRegression reg = new LinearRegression();
+        reg.addDataPoint(15, 10);
+        reg.addDataPoint(20, 20);
+        reg.regress();
+        assertEquals(9.7d, reg.getXIntercept());
+        assertEquals(-18.65, reg.getYIntercept(), 0.01d);
+        assertEquals(1.92d, reg.getSlope(), 0.01d);
+    }
+
+}
