c7509bf2b7f6141044dd1007a5efc6f38d3abf6b
Marko Srdanovic <marko.srdanovic@gmail.com>
2009-10-06 16:09:13 +0000

    Add some tests for "original args" in the UseModality class.
    git-svn-id: https://pbtech-vc.med.cornell.edu/public/svn/icb/trunk/icb-commons/icb-utils@20670 60cd2ccc-2b05-0410-9566-da273a940bff

--- test-src/edu/cornell/med/icb/cli/TestUseModality.java
+++ test-src/edu/cornell/med/icb/cli/TestUseModality.java
@@ -18,15 +18,26 @@
 
 package edu.cornell.med.icb.cli;
 
+import com.martiansoftware.jsap.FlaggedOption;
 import com.martiansoftware.jsap.JSAP;
 import com.martiansoftware.jsap.JSAPException;
 import com.martiansoftware.jsap.JSAPResult;
-import junit.framework.TestCase;
+import com.martiansoftware.jsap.Parameter;
+import org.apache.commons.lang.ArrayUtils;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
 /**
  * @author Fabien Campagne Date: Jul 8, 2007 Time: 11:29:41 AM
  */
-public class TestUseModality extends TestCase {
+public class TestUseModality {
+    private static final String[] ORIGINAL_ARGS =
+            {"--mode", "mode2", "--arg", "foo", "--arg", "bar", "-a", "baz"};
+
+    @Test
     public void testModality() throws JSAPException, IllegalAccessException,
             InstantiationException {
         final String[] args = {"--mode", "mode1", "--help"};
@@ -71,6 +82,59 @@ public class TestUseModality extends TestCase {
          */
         @Override
         public void process(final Object options) {
+        }
+    }
+
+
+    @Test
+    public void testOriginalArgs() throws InstantiationException, IllegalAccessException, JSAPException {
+        final UseModality<Object> mainModality2 = new UseModality<Object>() {
+            @Override
+            public void interpretArguments(final JSAP jsap,
+                                           final JSAPResult result,
+                                           final Object options) {
+            }
+
+            @Override
+            public void process(final Object options) {
+            }
+        };
+
+        mainModality2.setTestingMode(true);
+        mainModality2.registerMode("mode2", ModalityTest2.class);
+        mainModality2.process(ORIGINAL_ARGS, null);
+        assertArrayEquals(ORIGINAL_ARGS, mainModality2.getOriginalArgs());
+    }
+
+    public static class ModalityTest2 extends UseModality<Object> {
+        @Override
+        public void interpretArguments(final JSAP jsap,
+                                       final JSAPResult result,
+                                       final Object options) {
+            final String[] args = result.getStringArray("arg");
+            assertNotNull("args should not be null", args);
+            assertTrue("args must contain foo", ArrayUtils.contains(args, "foo"));
+            assertTrue("args must contain bar", ArrayUtils.contains(args, "bar"));
+            assertTrue("args must contain baz", ArrayUtils.contains(args, "baz"));
+            assertEquals("there should be exactly three args", 3, args.length);
+        }
+
+        @Override
+        public void process(final Object options) {
+            assertArrayEquals(ORIGINAL_ARGS, getOriginalArgs());
+        }
+
+        @Override
+        public void defineOptions(final JSAP jsap) throws JSAPException {
+            final Parameter parameter = new FlaggedOption("arg")
+                    .setStringParser(JSAP.STRING_PARSER)
+                    .setDefault(JSAP.NO_DEFAULT)
+                    .setRequired(false)
+                    .setAllowMultipleDeclarations(true)
+                    .setShortFlag('a')
+                    .setLongFlag("arg")
+                    .setHelp("testing mutilple args");
+            jsap.registerParameter(parameter);
         }
     }
 }
